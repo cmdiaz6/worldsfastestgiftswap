1dd6a1728b6c9d47e927ef660483d76fe43eb733
Carlos on Mac <disreprivalize@gmail.com>
2020-11-27 12:50:18 -0700

    command line arguments and option to read names from file for output

--- giftswap.f90
+++ giftswap.f90
@@ -13,29 +13,161 @@ module errorcheckmod
             !stop
         end if
     end function
+
+end module
+
+module io_mod
+    implicit none
+    contains
+    subroutine readList(filename, groupSize, partnerSize, name_list, partner_list, ierr)
+
+        character(len=*),intent(IN) :: filename
+
+        integer,intent(OUT) :: groupSize, partnerSize
+        character(len=*),allocatable,intent(OUT) :: name_list(:)
+        integer,allocatable,intent(OUT) :: partner_list(:)
+        integer,intent(OUT) :: ierr
+
+        integer :: iunit, stat, icount
+        logical :: readPartners
+
+        ! check if file exists
+        inquire(file=filename,exist=readPartners)
+        if (.not.readPartners) then
+            ierr=-1
+            return
+        end if
+
+        iunit=79902
+        open(unit=iunit, file=filename)
+        read(iunit,*,iostat=ierr) groupSize, partnerSize
+        if (ierr/=0) return
+
+        allocate(partner_list(groupSize))
+        allocate(   name_list(groupSize))
+
+        readPartners=.true.
+        ierr=0
+        icount=1
+        partner_list(:)=0
+        readfile: do
+            ! check if done reading partners
+            if (icount >= 2*partnerSize) readPartners=.false.
+            if (readPartners) then
+                read(iunit, *, iostat=stat) name_list(icount), name_list(icount+1)
+            else
+                read(iunit,*, iostat=stat) name_list(icount)
+            end if
+            if (stat==0) then
+                if (readPartners) then
+                    partner_list(icount)=icount+1
+                    partner_list(icount+1)=icount
+                    icount=icount+2
+                else
+                    icount=icount+1
+                end if
+            else
+                print *,'Error reading',filename
+                ierr=-1
+            end if
+            if (icount > groupSize) exit readfile
+        end do readfile
+        close(iunit)
+
+    end subroutine readList
+    subroutine get_arguments(groupSize, partnerSize, filename, readFromFile, ierr)
+        integer,intent(INOUT) :: groupSize, partnerSize
+        character(len=*),intent(INOUT) :: filename
+        logical,intent(OUT) :: readFromFile
+        integer,intent(OUT) :: ierr
+
+        integer :: iarg
+        character(len=32) :: opt, arg
+
+        ierr = 0
+        readFromFile = .false.
+        do iarg = 1, command_argument_count(), 2
+            call get_command_argument(iarg, opt)
+            call get_command_argument(iarg+1, arg, status=ierr)
+            select case(opt)
+                case('-n')
+                    read(arg,*,iostat=ierr) groupSize
+                case('-c')
+                    read(arg,*,iostat=ierr) partnerSize
+                case('-f','--filename')
+                    filename = arg
+                    readFromFile = .true.
+                case default
+                    ierr=-1
+            end select
+            if (ierr /=0) then
+                call print_options
+                exit
+            end if
+        end do
+    end subroutine
+    subroutine print_options
+        print *,'Command-line options:'
+        print *,'  -f, --filename     file with participant names. See README for format'
+        print *,'  -n                 total number of participants'
+        print *,'  -c                 total number of couples.' 
+        print *,'                     Must be <= (1/2)*number of participants'
+    end subroutine
 end module
 
 program giftswap
     use errorcheckmod
+    use io_mod
     implicit none
-    integer, parameter :: gift_group_size = 8
-    !integer, parameter :: gift_group_size = 1000000
     integer, parameter :: max_checks = 1000
+    !integer, parameter :: gift_group_size = 100000000
+    integer :: gift_group_size = 8
+    integer :: number_of_couples = 4
     real    :: rand
-    integer :: give_list(gift_group_size), partner_list(gift_group_size)
-    integer :: recv_list(gift_group_size), free_list(gift_group_size)
+    integer,allocatable :: give_list(:), partner_list(:)
+    integer,allocatable :: recv_list(:), free_list(:)
+    character(len=100),allocatable :: name_list(:)
     integer :: last_spot, random_spot
-    integer :: gift_giver, random_person, icheck, ierr, nchecks
-    logical :: partner_assigned, running
+    integer :: gift_giver, random_person, icheck, ierr
+    logical :: partner_assigned, running, readFromFile
     integer :: iunit
     character(len=100) :: fmt1
+    character(len=11)  :: filename='partnerlist'
+
+    ! input options
+    ! -f filename
+    ! if not reading names from file:
+    ! -n gift_group_size
+    ! -p number_of_couples
+    call get_arguments(gift_group_size, number_of_couples, filename, readFromFile, ierr)
+    if (ierr/=0) stop
     
+    ! read participants from file
+    ! place partners on same line
+    if (readFromFile) then
+        call readList(filename, gift_group_size, number_of_couples, name_list, partner_list, ierr)
+        if (ierr /= 0 ) readFromFile = .false.
+    end if
+
+    ! manual entry
+    if (.not.readFromFile) then
+        allocate(partner_list(gift_group_size))
+        !partner_list(1:8)=[2,1, 4,3, 6,5, 8,7]
+        do gift_giver = 1, 2*number_of_couples, 2
+            partner_list(gift_giver)=gift_giver+1
+            partner_list(gift_giver+1)=gift_giver
+        end do
+    end if    
+
+    allocate(give_list(gift_group_size))
+    allocate(recv_list(gift_group_size))
+    allocate(free_list(gift_group_size))
+
     running=.true.
     restart: do while (running) ! itry=1,max_checks
         do gift_giver=1,gift_group_size
             free_list(gift_giver)=gift_giver
         end do
-        partner_list(1:8)=[2,1, 4,3, 6,5, 8,7]
         
         give_list(:)=0
         recv_list(:)=0
@@ -87,13 +219,22 @@ program giftswap
         running=.false. ! finished. stop running
     end do restart
 
-    print *,'Printing gift give list'
-    iunit=915
+!    stop
+    print *,'Printing gift give list to output.txt'
+    iunit=79902
     open(unit=iunit,file='output.txt',form='formatted')
     fmt1="(A,I7,A,I7)"
+    if (readFromFile) fmt1="(A,A,A,A)"
     do gift_giver=1,gift_group_size
-        write(iunit,fmt1) 'member ',gift_giver,' gives to member ',give_list(gift_giver)
-!        write(fmt1,6) name_list(gift_giver), name_list(give_list(gift_giver))
+        if (readFromFile) then
+            write(iunit,fmt1) trim(name_list(gift_giver)),' gives to ',trim(name_list(give_list(gift_giver)))
+            if (gift_group_size <= 20) &
+                write(6,fmt1) trim(name_list(gift_giver)),' gives to ',trim(name_list(give_list(gift_giver)))
+        else
+            write(iunit,fmt1) 'member ',gift_giver,' gives to member ',give_list(gift_giver)
+            if (gift_group_size <= 20) &
+                write(*,fmt1) 'member ',gift_giver,' gives to member ',give_list(gift_giver)
+        end if
     end do
     close(iunit)
 end program
